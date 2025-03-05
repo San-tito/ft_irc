f26df001a54802ed23efd5286e4fc9b95a9bb93b
San-tito <68120901+San-tito@users.noreply.github.com>
2025-03-05 15:44:37 +0100

    ipv4/ipv6 handling.

--- src/Conn.cpp
+++ src/Conn.cpp
@@ -6,24 +6,33 @@
 /*   By: sguzman <sguzman@student.42barcelona.com   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 14:57:02 by sguzman           #+#    #+#             */
-/*   Updated: 2025/03/05 15:03:42 by sguzman          ###   ########.fr       */
+/*   Updated: 2025/03/05 15:43:10 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Conn.hpp"
 
+static bool	InitAddr(struct sockaddr_in *addr, const char *ip,
+		unsigned short port)
+{
+	bzero(addr, sizeof(*addr));
+	addr->sin_family = AF_INET;
+	addr->sin_addr.s_addr = inet_addr(ip);
+	if (addr->sin_addr.s_addr == (unsigned)-1)
+		return (false);
+	addr->sin_port = htons(port);
+	return (true);
+}
+
 int Conn::NewListener(const char *listen_addr, unsigned short port)
 {
 	struct sockaddr_in	addr;
 
-	bzero(&addr, sizeof(addr));
-	addr.sin_family = AF_INET;
-	if (inet_aton(listen_addr, &addr.sin_addr) == 0)
+	if (!InitAddr(&addr, listen_addr, port))
 	{
 		std::cerr << "Can't listen on [" << listen_addr << "]:" << port << ": Failed to parse IP address!\n";
 		return (-1);
 	}
-	addr.sin_port = htons(port);
 	int af(addr.sin_family);
 	int sock(socket(af, SOCK_STREAM, 0));
 	if (sock < 0)
