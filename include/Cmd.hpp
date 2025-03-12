5af8948c38839353c21a0dd6215a2ee5c8ca4499
Deivid Santiago Ordoñez Varela <deordone@cbr15s5.42barcelona.com>
2025-03-12 19:55:50 +0100

    ahora si finish esructura

--- include/Cmd.hpp
+++ include/Cmd.hpp
@@ -1,18 +1,23 @@
-#pragma once
+#ifndef CMD_HPP
+#define CMD_HPP
 
-# include "Client.hpp"
+#include "Client.hpp"
 #include <iostream>
 #include <map>
 #include <vector>
 
-class Cmd 
+class Cmd
 {
-	private:
-		void	Join(int client_fd, std::vector<std::string> name);
-		// void	Nick(int client_fd, const std::string& name);
-		// void	PrivMsg(int client_fd, const std::string& name);
-		// void	User(int client_fd, const std::string& name);
-	public:
-		Cmd();
-		static std::map<std::string, void(Cmd::*)(int, std::vector<std::string>)> commands;  
+  private:
+	static void Join(int client_fd, std::vector<std::string> name);
+	// void	Nick(int client_fd, const std::string& name);
+	// void	PrivMsg(int client_fd, const std::string& name);
+	// void	User(int client_fd, const std::string& name);
+  public:
+	static void Init(void);
+	static std::map<std::string, void (*)(int,
+		std::vector<std::string>)> commands;
 };
+
+
+#endif /* CMD_HPP */
--- src/Cmd.cpp
+++ src/Cmd.cpp
@@ -1,19 +1,17 @@
 #include "Cmd.hpp"
 
-std::map<std::string, void(Cmd::*)(int, std::vector<std::string>)> Cmd::commands;  
+std::map<std::string, void (*)(int, std::vector<std::string>)> Cmd::commands;
 
-Cmd::Cmd()
+void Cmd::Init(void)
 {
-	commands.insert(std::make_pair("JOIN", &Cmd::Join));
+	commands["JOIN"] = Join;
 }
 
 void Cmd::Join(int client_fd, std::vector<std::string> params)
 {
 	typedef std::vector<std::string>::iterator iterator;
-
 	std::cout << "En efecto yo soy el " << client_fd << " y tengo estos parametros:\n";
 	for (iterator it(params.begin()); it != params.end(); it++)
 		std::cout << *it << "\n";
 	std::cout << "\n";
 }
-
