012fd356d76e2891b0cf0a5a3664a7e47bbad252
droied <deiord44santiagoov@gmail.com>
2025-03-14 11:50:50 +0100

    polish changes

--- src/Cmd.cpp
+++ src/Cmd.cpp
@@ -17,7 +17,7 @@ void Cmd::Pass(Client& client, std::vector<std::string> params)
 
 	if (params.size() != 1)
 	{
-		Log::Err() << "PASS: Syntax Error";
+		client << "PASS: Syntax Error";
 		return ;
 	}
 	if (client.getPassword().empty())
@@ -34,18 +34,26 @@ void Cmd::Nick(Client& client, std::vector<std::string> params)
 	typedef std::vector<std::string>::iterator iterator;
 
 	iterator user_nick(params.begin());
-
-	//it remains to check if the Nick given its unique
+	std::vector<Client> clients = Server::getClients();
 	
 	if (params.size() != 1)
 	{
-		Log::Err() << "NICK: Syntax Error";
+		client << "NICK: Syntax Error";
 		return ;
 	}
 	if (user_nick->size() >= 9)
 	{
 		client << *user_nick << " :Nickname too long, max. 9 characters";
 		return ;
+	}
+	//falta testear esto en principio si dos clientes se conectan con el mismo nickname deberia fallar.
+	for (std::vector<Client>::iterator it(clients.begin()); it != clients.end(); it++)
+	{
+		if (!user_nick->compare(it->getNickname()))
+		{
+			client << *user_nick << " :Nickname already in use";
+			return ;
+		}
 	}
 	client.setNickname(*user_nick);
 	Log::Info() << "got valid NICK command";
@@ -57,9 +65,15 @@ void Cmd::User(Client& client, std::vector<std::string> params)
 
 	iterator user(params.begin());
 	
+	if (params.size() < 4)
+	{
+		client << "USER: Syntax Error";
+		return ;
+	}
+	
+	// check the 2 and 3 parameter it should be just one byte
+
 	//it should be USER deo 0 * : tuta
-	// but u can pass even this USER deo a a :real
-	// and the realname may contain spaces
 
 	client.setUser(*user);
 	Log::Info() << "got valid USER command";
