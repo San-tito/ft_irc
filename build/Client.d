f771db28eed74af23bb105baf8816091a911f9cc
San-tito <68120901+San-tito@users.noreply.github.com>
2025-03-20 23:58:15 +0100

    KICK

--- build/Client.d
+++
@@ -1,4 +0,0 @@
-build/Client.o: src/Client.cpp include/Client.hpp include/Server.hpp \
- include/Channel.hpp include/Client.hpp include/Membership.hpp \
- include/Cmd.hpp include/Parser.hpp include/Log.hpp include/Conn.hpp \
- include/Messages.hpp include/Sig.hpp
